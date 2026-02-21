3f2564bedd75950df3468cf68beb6ee6fb87caac
Alexander <akochnev66@gmail.com>
2026-02-21 10:35:34 +0300

    add kanban

--- update_cert.sh
+++ update_cert.sh
@@ -2,9 +2,9 @@
 
 # обновление cretificate
 docker compose run --rm certbot_init certonly --webroot --webroot-path=/var/www/certbot \
+      --expand \
       -d abc8888.ru \
       -d api.abc8888.ru \
       -d test.abc8888.ru \
       -d kan.abc8888.ru \
-      -d api.test.abc8888.ru \
-      --expand;
+      -d api.test.abc8888.ru;
