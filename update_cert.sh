698a8b8ca91f37830b66694bc3870799e75ddc59
Alexander <akochnev66@gmail.com>
2026-03-09 04:12:52 +0300

    update_cert.sh

--- update_cert.sh
+++ update_cert.sh
@@ -7,4 +7,5 @@ docker compose run --rm certbot_init certonly --webroot --webroot-path=/var/www/
       -d api.abc8888.ru \
       -d test.abc8888.ru \
       -d kan.abc8888.ru \
-      -d api.test.abc8888.ru;
+      -d api.test.abc8888.ru \
+      -d netdata.test.abc8888.ru;
