d5fc34454d376c43df807a1c44f045fbf783bef1
Alexander <akochnev66@gmail.com>
2026-04-03 16:01:45 +0300

    subnet added

--- network.sh
+++ network.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker network create shared_network
-docker network create clickhouse_network
-docker network create clickhousepg_network
-docker network create vllm_network
+docker network create --driver bridge --subnet 172.90.10.0/24 shared_network
+docker network create --driver bridge --subnet 172.90.20.0/24 clickhouse_network
+docker network create --driver bridge --subnet 172.90.30.0/24 clickhousepg_network
+docker network create --driver bridge --subnet 172.90.40.0/24 vllm_network
